a8a8708f6d1df47f90679aaabe9b7713c10f57ae
Bruno Dorscheidt Brandelli <bdbrandelli@hotmail.com>
2017-11-16 00:40:30 -0200

    Update normalize.js

--- normalize.js
+++ normalize.js
@@ -1,12 +1,21 @@
-function getNormalized(){
-  var $dl, $font;
-  $dl = document.querySelectorAll('dl');
-  for(var k in $dl){
-    $font = $dl.item(k).querySelectorAll('font');
-    for(var j in $font){
-      var teste = $font[j].innerHTML;
-      if(teste[0] == '[')
-        console.log(teste);
+  function getNormalized(){
+    var $dl, $font, strResult, teste, testinho;
+    $dl = document.querySelectorAll('dl');
+    for(var k in $dl){
+      $font = $dl.item(k).querySelectorAll('font');
+      for(var j in $font){
+        strResult = "";
+        teste = $font[j].innerHTML;
+        if(teste[0] == '['){
+          console.log(teste);
+        }else{
+          if(j>0){
+            testinho = $font[j].querySelectorAll('b');
+            if(!!testinho && !!testinho[0]){
+              console.log(testinho[0].textContent);
+            }
+          }
+        }
+      }
     }
   }
-}
