86a6f7ff719516989e1af62c8e05b30ac3f369cf
brandelli <bdbrandelli@hotmail.com>
2017-11-18 17:53:30 -0200

    teste

--- normalize.js
+++ normalize.js
@@ -1,21 +1,25 @@
   function getNormalized(){
     var $dl, $font, strResult, teste, testinho;
+    strResult = "";
     $dl = document.querySelectorAll('dl');
-    for(var k in $dl){
+    for(var k = 0; k < $dl.length ; k++){
       $font = $dl.item(k).querySelectorAll('font');
+      var nWords = 0
       for(var j in $font){
-        strResult = "";
         teste = $font[j].innerHTML;
-        if(teste[0] == '['){
-          console.log(teste);
+        if(!!teste && teste[0] == '[' && teste != '[,]'){
+          strResult = strResult + teste;
         }else{
           if(j>0){
             testinho = $font[j].querySelectorAll('b');
-            if(!!testinho && !!testinho[0]){
-              console.log(testinho[0].textContent);
+            if(!!testinho && !!testinho[0] && testinho[0].innerHTML[0] != '@'){
+              strResult = strResult +" "+ testinho[0].innerHTML + "\n";
+              j = $font.lenght + 1;
             }
           }
         }
       }
     }
+    console.log(strResult);
   }
+
